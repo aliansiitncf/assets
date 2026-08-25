2061aeb27f7e1c7cb8aa1dcd4606b8b7443f71e5
adityarifkiy16 <adityarifkistemba@gmail.com>
2026-08-25 19:44:01 +0700

    fix: restore inline component modal logic for asset repair

--- resources/views/livewire/asset/asset-repair-edit.blade.php
+++ resources/views/livewire/asset/asset-repair-edit.blade.php
@@ -411,6 +411,40 @@
             </div>
         </form>
 
-        @livewire('components.component-modal')
+        
+    @if($isOpen)
+    <div class="modal modal-open">
+        <div class="modal-box">
+            <button type="button" wire:click="$set('isOpen', false)" class="btn btn-sm btn-circle absolute right-2 top-2">✕</button>
+            <h3 class="font-bold text-lg mb-4">
+                {{ $componentId ? 'Edit Component' : 'Add Component' }}
+            </h3>
+
+            <form wire:submit="save">
+                <div class="form-control mb-4">
+                    <label class="label">Nama Component</label>
+                    <input type="text" wire:model="name_component" class="input input-bordered w-full" autofocus />
+                    @error('name_component')
+                    <span class="text-error text-sm">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="modal-action">
+                    <button type="button" class="btn btn-secondary" wire:click="$set('isOpen', false)">
+                        Batal
+                    </button>
+                    <button type="submit" class="btn btn-primary" wire:loading.attr="disabled">
+                        <span wire:loading.remove>
+                            {{ $componentId ? 'Update' : 'Simpan' }}
+                        </span>
+                        <span wire:loading>Loading...</span>
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="modal-backdrop" wire:click="$set('isOpen', false)"></div>
+    </div>
+    @endif
+
     </div>
 </div>
